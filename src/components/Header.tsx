b24c5e2a2a25225e5e0ed46e44163071cdae2a3c
eobcre <eobcre@gmail.com>
2023-07-24 09:11:18 -0400

    created header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+type HeaderProps = {
+  text: string;
+};
+
+const Header: React.FC<HeaderProps> = (props) => {
+  return (
+    <header>
+      <h1>{props.text}</h1>
+    </header>
+  );
+};
+
+export default Header;

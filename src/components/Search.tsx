38bb5f462803f80ffdb5e62985d93406c7e71d31
eobcre <eobcre@gmail.com>
2023-07-25 10:00:59 -0400

    scss imported, icon added

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,11 @@
+import '../scss/_search.scss';
+import { BiSearch } from 'react-icons/bi';
+
 const Search = () => {
   return (
-    <div>
+    <div className='container'>
       <input type='text' />
+      <BiSearch size={30} />
     </div>
   );
 };

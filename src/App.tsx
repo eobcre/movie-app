c097a0b8f483549683d4818118ae1c3168b0b072
eobcre <eobcre@gmail.com>
2023-07-25 09:37:55 -0400

    added fragments

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import Header from './components/Header';
 
 const App: React.FC = () => {
-  return <Header text='Movie App' />;
+  return (
+    <>
+      <Header text='Movie App' />
+    </>
+  );
 };
 
 export default App;

6d8d248940da472c922456b29aefd9bbbb3c73e1
eobcre <eobcre@gmail.com>
2023-07-25 09:40:03 -0400

    added search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from './components/Header';
+import Search from './components/Search';
 
 const App: React.FC = () => {
   return (
     <>
       <Header text='Movie App' />
+      <Search />
     </>
   );
 };

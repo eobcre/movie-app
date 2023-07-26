ec7b1f58b6e57f2f6ecc9af2c57349f970c3009e
eobcre <eobcre@gmail.com>
2023-07-26 12:52:44 -0400

    created search layout and button click test

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,10 +2,16 @@ import '../scss/_search.scss';
 import { BiSearch } from 'react-icons/bi';
 
 const Search = () => {
+  const handleClick = () => {
+    console.log('Test');
+  };
+
   return (
-    <div className='container'>
-      <input type='text' />
-      <BiSearch size={30} />
+    <div className='search-container'>
+      <input type='text' className='search-input' />
+      <button onClick={handleClick} className='search-button'>
+        <BiSearch className='search-icon' />
+      </button>
     </div>
   );
 };

d1e7279ace484afb766749ec7977645c2919987c
eobcre <eobcre@gmail.com>
2023-07-26 23:17:30 -0400

    removed prop type

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,7 @@
 import '../Header/Header.scss';
 import Logo from '../../assets/logo.png';
 
-type HeaderProps = {
-  text: string;
-};
-
-const Header: React.FC<HeaderProps> = (props) => {
+const Header: React.FC = () => {
   return (
     <header>
       <img src={Logo} alt='Logo' width={400} />

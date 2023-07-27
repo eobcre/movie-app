3072c9022d394e9d8f2bb768f977009784bf3468
eobcre <eobcre@gmail.com>
2023-07-26 23:10:57 -0400

    created folder for component and scss

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import '../Header/Header.scss';
+import Logo from '../../assets/logo.png';
+
+type HeaderProps = {
+  text: string;
+};
+
+const Header: React.FC<HeaderProps> = (props) => {
+  return (
+    <header>
+      <img src={Logo} alt='Logo' width={400} />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import '../Search/Search.scss';
+import { BiSearch } from 'react-icons/bi';
+
+const Search: React.FC = () => {
+  const [search, setSearch] = useState<string>('');
+
+  const handleOnChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const handleOnClickButton = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form>
+      <div className='search-container'>
+        <input
+          type='text'
+          onChange={handleOnChangeInput}
+          value={search}
+          className='search-input'
+        />
+        <button onClick={handleOnClickButton} className='search-button'>
+          <BiSearch className='search-icon' />
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Search;

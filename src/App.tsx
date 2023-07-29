18d98423df8e7f88637cc3d70c4b6c80715c21f5
eobcre <eobcre@gmail.com>
2023-07-28 22:36:31 -0400

    api logic, display movies

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
+import { useState, useEffect } from 'react';
+
 import Header from './components/Header/Header';
 import Search from './components/Search/Search';
+import Movie from './components/Movie/Movie';
 
 const App: React.FC = () => {
-  const API_KEY = import.meta.env.VITE_APP_API_KEY;
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState<Movie[]>([]);
 
+  // API KEY
+  const API_KEY = import.meta.env.VITE_APP_API_KEY;
   const API_URL = `https://www.omdbapi.com/?s=man&apikey=${API_KEY}`;
+
+  // calling API once
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.Search);
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <>
       <Header />
       <Search />
+      <div className='movies'>
+        {movies.map((movie, index) => (
+          <Movie key={`${index}-${movie.Title}`} movie={movie} />
+        ))}
+      </div>
     </>
   );
 };

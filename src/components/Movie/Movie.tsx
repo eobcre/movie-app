c6edc82bd6f6bfdb73377b6264f270f1d57303fd
eobcre <eobcre@gmail.com>
2023-07-28 21:35:57 -0400

    changed default poster image variable due to readability

--- src/components/Movie/Movie.tsx
+++ src/components/Movie/Movie.tsx
@@ -12,10 +12,10 @@ type MovieProps = {
 
 const Movie: React.FC<MovieProps> = ({ movie }) => {
   // console.log(movie);
-  const DEFAULT_IMAGE =
+  const DEFAULT_POSTER =
     'https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg';
 
-  const poster = movie.Poster === 'N/A' ? DEFAULT_IMAGE : movie.Poster;
+  const poster = movie.Poster === 'N/A' ? DEFAULT_POSTER : movie.Poster;
 
   return (
     <div className='movie-container'>

6bfcb4d8da1a095cfdee955d931886846e96d981
eobcre <eobcre@gmail.com>
2023-07-28 21:34:33 -0400

    movie poster images display logic, layout created and prop types

--- src/components/Movie/Movie.tsx
+++ src/components/Movie/Movie.tsx
@@ -1,19 +1,27 @@
-type MovieProps = {
-  props: {
-    title: string;
-    year: number;
-    poster: string;
-  };
+import '../Movie/Movie.scss';
+
+type Movie = {
+  Title: string;
+  Year: string;
+  Poster: string;
 };
 
-const Movie: React.FC<MovieProps> = ({ props }) => {
+type MovieProps = {
+  movie: Movie;
+};
+
+const Movie: React.FC<MovieProps> = ({ movie }) => {
+  // console.log(movie);
+  const DEFAULT_IMAGE =
+    'https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg';
+
+  const poster = movie.Poster === 'N/A' ? DEFAULT_IMAGE : movie.Poster;
+
   return (
-    <div className='movie'>
-      <h2>{props.title}</h2>
-      <div>
-        <img width='200' alt={`${props.title}`} />
-      </div>
-      <p>({props.year})</p>
+    <div className='movie-container'>
+      <p>{movie.Title}</p>
+      <p>({movie.Year})</p>
+      <img width='200' alt={`${movie.Title}`} src={poster} />
     </div>
   );
 };

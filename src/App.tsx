e9ebe91a08df19faf78805a36101d2ddaf017e14
eobcre <eobcre@gmail.com>
2023-07-29 12:13:59 -0400

    loading and error message logic, search movies logic, JSX embeded

--- src/App.tsx
+++ src/App.tsx
@@ -7,29 +7,59 @@ import Movie from './components/Movie/Movie';
 const App: React.FC = () => {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   // API KEY
   const API_KEY = import.meta.env.VITE_APP_API_KEY;
   const API_URL = `https://www.omdbapi.com/?s=man&apikey=${API_KEY}`;
 
-  // calling API once
+  // retrieve API once
   useEffect(() => {
     fetch(API_URL)
       .then((res) => res.json())
       .then((data) => {
+        // console.log(data);
         setMovies(data.Search);
         setLoading(false);
       });
   }, []);
 
+  // search movies
+  const searchFunc = (searchQuery: string) => {
+    setLoading(true);
+    setErrorMessage(null);
+
+    // API KEY
+    const API_SEARCH_URL = `https://www.omdbapi.com/?s=${searchQuery}&apikey=${API_KEY}`;
+
+    fetch(API_SEARCH_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.Response === 'True') {
+          setMovies(data.Search);
+          setLoading(false);
+        } else {
+          setErrorMessage(data.Error);
+          setLoading(false);
+        }
+      });
+  };
+
   return (
     <>
       <Header />
-      <Search />
+      <Search searchFunc={searchFunc} />
       <div className='movies'>
-        {movies.map((movie, index) => (
-          <Movie key={`${index}-${movie.Title}`} movie={movie} />
-        ))}
+        {loading && !errorMessage ? (
+          <span className='loading-text'>Loading...</span>
+        ) : !loading && errorMessage ? (
+          <p>{errorMessage}</p>
+        ) : (
+          movies.map((movie, index) => (
+            <Movie key={`${index}-${movie.Title}`} movie={movie} />
+          ))
+        )}
       </div>
     </>
   );

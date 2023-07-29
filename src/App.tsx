e1dd81b93600ce2f69a0482293dd9742be1a2639
eobcre <eobcre@gmail.com>
2023-07-28 22:13:15 -0400

    api key settings

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import Header from './components/Header/Header';
 import Search from './components/Search/Search';
 
 const App: React.FC = () => {
+  const API_KEY = import.meta.env.VITE_APP_API_KEY;
+
+  const API_URL = `https://www.omdbapi.com/?s=man&apikey=${API_KEY}`;
+
   return (
     <>
       <Header />

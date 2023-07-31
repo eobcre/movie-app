ad6de6ba7c4c76887ab6397a887f12a0c1c4b210
eobcre <eobcre@gmail.com>
2023-07-31 18:17:38 -0400

    logo width changed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import Logo from '../../assets/logo.png';
 const Header: React.FC = () => {
   return (
     <header>
-      <img src={Logo} alt='Logo' width={400} />
+      <img src={Logo} alt='Logo' width={180} />
     </header>
   );
 };

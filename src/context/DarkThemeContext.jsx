3a60666e521ee2da3ab3ab15488fa06bf762cf63
eobcre <eobcre@gmail.com>
2023-08-04 19:48:30 -0400

    created provider and jsx

--- src/context/DarkThemeContext.jsx
+++ src/context/DarkThemeContext.jsx
@@ -1,3 +1,19 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
+// create context
 const DarkThemeContext = createContext();
+
+// create provider
+const DarkThemeProvider = ({ children }) => {
+  const [isDark, setIsDark] = useState(false);
+
+  return (
+    <>
+      <DarkThemeContext.Provider value={{ isDark, setIsDark }}>
+        {children}
+      </DarkThemeContext.Provider>
+    </>
+  );
+};
+
+export { DarkThemeContext, DarkThemeProvider };

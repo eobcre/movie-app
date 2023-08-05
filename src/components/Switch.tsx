86caeca4fa274b03f6ab844d36892ad0368344d4
eobcre <eobcre@gmail.com>
2023-08-05 14:03:50 -0400

    null check for state variables, switch icons logic

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -1,16 +1,32 @@
 import { useContext } from 'react';
 import { DarkThemeContext } from '../context/DarkThemeContext';
 
-const Switch: React.FC = () => {
-  const { isDark, setIsDark } = useContext(DarkThemeContext);
+import { TiWeatherSunny, TiWeatherNight } from 'react-icons/ti';
 
+const Switch: React.FC = () => {
+  const ThemeContext = useContext(DarkThemeContext);
+
+  // null check
+  if (!ThemeContext) {
+    throw new Error('DarkThemeContext is null.');
+  }
+
+  const { isDark, setIsDark } = ThemeContext;
+
+  // click logic
   const handleSwitch = () => {
     setIsDark(!isDark);
   };
 
   return (
     <div>
-      <div onClick={handleSwitch}></div>
+      <div onClick={handleSwitch}>
+        {isDark ? (
+          <TiWeatherSunny className='icon-light' />
+        ) : (
+          <TiWeatherNight className='icon-dark' />
+        )}
+      </div>
     </div>
   );
 };
